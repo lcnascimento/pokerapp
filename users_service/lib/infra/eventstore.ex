18892f8226b63aa14bcd6e1e9bba2ec1869bac50
Luís Nascimento <luisnascimento.dev@gmail.com>
2020-04-19 14:37:23 -0300

    evenstore as GenServer

--- users_service/lib/infra/eventstore.ex
+++ users_service/lib/infra/eventstore.ex
@@ -1,16 +1,35 @@
 defmodule UsersService.Infra.Eventstore do
   alias UsersService.Infra.Event
-  alias UsersService.Infra.Error
 
-  def connect do
-    {:ok, conn} = Xandra.start_link(nodes: ["127.0.0.1:9043"])
-    conn
+  use GenServer
+
+  # Client side
+
+  def start_link(default \\ %{}) do
+    GenServer.start(__MODULE__, default)
   end
 
-  @spec insert(event :: Event.t()) :: any()
-  def insert(event) do
-    conn = connect()
+  @spec insert(pid :: pid(), event :: Event.t()) :: any()
+  def insert(pid, event) do
+    GenServer.call(pid, {:insert, event})
+  end
 
+  @spec stream(pid :: pid(), row_id :: String.t(), agg_id :: String.t()) :: any()
+  def stream(pid, row_id \\ nil, agg_id \\ nil) do
+    GenServer.call(pid, {:stream, row_id, agg_id})
+  end
+
+  # Server side
+
+  @impl GenServer
+  def init(state) do
+    {:ok, conn} = Xandra.start_link(nodes: cassandra_nodes())
+
+    {:ok, Map.put(state, :conn, conn)}
+  end
+
+  @impl GenServer
+  def handle_call({:insert, event}, _from, %{conn: conn} = state) do
     statement =
       "INSERT INTO users_service.users(row_id, aggregate_id, event_time, event_type, payload) VALUES (?, ?, ?, ?, ?)"
 
@@ -23,44 +42,43 @@ defmodule UsersService.Infra.Eventstore do
         {"map<text,text>", event.payload}
       ])
 
-    response
+    {:reply, response, state}
   end
 
-  @spec stream(row_id :: String.t(), agg_id :: String.t()) :: any()
-  def stream(row_id \\ nil, agg_id \\ nil)
-
-  def stream(row_id, agg_id) when row_id == nil and agg_id == nil do
-    conn = connect()
-
+  @impl GenServer
+  def handle_call({:stream, row_id, agg_id}, _from, %{conn: conn} = state)
+      when row_id == nil and agg_id == nil do
     query = "SELECT * FROM users_service.users"
     statement = Xandra.prepare!(conn, query)
 
-    Xandra.stream_pages!(conn, statement, [], page_size: 1_000)
+    stream = Xandra.stream_pages!(conn, statement, [], page_size: 1_000)
+
+    {:reply, stream, state}
   end
 
-  def stream(row_id, agg_id) when agg_id == nil do
-    conn = connect()
-
+  @impl GenServer
+  def handle_call({:stream, row_id, agg_id}, _from, %{conn: conn} = state)
+      when row_id != nil and agg_id == nil do
     query = "SELECT * FROM users_service.users WHERE row_id = ?"
     statement = Xandra.prepare!(conn, query)
 
-    Xandra.stream_pages!(conn, statement, [row_id], page_size: 1_000)
+    stream = Xandra.stream_pages!(conn, statement, [row_id], page_size: 1_000)
+
+    {:reply, stream, state}
   end
 
-  def stream(row_id, agg_id) do
-    conn = connect()
-
+  @impl GenServer
+  def handle_call({:stream, row_id, agg_id}, _from, %{conn: conn} = state)
+      when row_id != nil and agg_id != nil do
     query = "SELECT * FROM users_service.users WHERE row_id = ? AND aggregate_id = ?"
     statement = Xandra.prepare!(conn, query)
 
-    Xandra.stream_pages!(conn, statement, [row_id, agg_id], page_size: 1_000)
+    stream = Xandra.stream_pages!(conn, statement, [row_id, agg_id], page_size: 1_000)
+
+    {:reply, stream, state}
   end
 
-  def snapshoot() do
-    {:error, %{reason: "NotImplemented"}}
-  end
-
-  def last_snapshoot() do
-    {:error, %{reason: "NotImplemented"}}
+  defp cassandra_nodes do
+    ["127.0.0.1:9043"]
   end
 end
